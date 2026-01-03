8c644cab7618214947166cd70e23a3c05a209536
breakfastpasta <breakfastpasta@protonmail.com>
2026-01-02 22:10:46 -0700

    Add prune collections config

--- Jellyfin.Plugin.AnimeSeasons/Configuration/PluginConfiguration.cs
+++ Jellyfin.Plugin.AnimeSeasons/Configuration/PluginConfiguration.cs
@@ -3,5 +3,10 @@ using MediaBrowser.Model.Plugins
 namespace Jellyfin.Plugin.AnimeSeasons.Configuration;
 class PluginConfiguration : BasePluginConfiguration;
 {
+    public PluginConfiguration()
+    {
+        PruneOldCollections = false;
+    }
 
+    public int PruneOldCollections { get; set; }
 }
